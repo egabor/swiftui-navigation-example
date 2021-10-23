fdb678e97e8fcc3e2e984cd4a859b8401270e2cf
Eszenyi Gábor <eszenyi.gabor@icloud.com>
2021-10-23 18:38:50 +0200

    Initial Commit

---
+++ swiftui-navigation-example/swiftui_navigation_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_navigation_exampleApp.swift
+//  swiftui-navigation-example
+//
+//  Created by Eszenyi Gábor on 2021. 10. 23..
+//
+
+import SwiftUI
+
+@main
+struct swiftui_navigation_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
